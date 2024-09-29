17ac5b0ade43fcb1b51493d1566a6647eb48a04f
kousra <mdkousarmia71@gmail.com>
2024-09-29 13:50:49 +0600

    first commit

---
+++ scope.js
@@ -0,0 +1,22 @@
+// const x = 5;
+
+// console.log(x);
+
+// if(true){
+//     console.log(x);
+    
+// }
+
+// function show() {
+//     console.log(x);
+    
+// }
+// show()
+
+{
+    const x = 10;
+    console.log(x);
+    
+}
+
+console.log(x);
