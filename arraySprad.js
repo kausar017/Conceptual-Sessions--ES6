13f2de29c5cebc55542e084d52c4e96fb8944754
kousra <mdkousarmia71@gmail.com>
2024-09-30 06:40:34 +0600

    js practis

--- arraySprad.js
+++ arraySprad.js
@@ -1,3 +1,6 @@
+
+
+//[arraySprid ES6]
 // const arr1 = [1, 2, 3];
 
 // const arr2 = [...arr1];
@@ -8,3 +11,15 @@
 // console.log(arr2);
 // console.log(arr1);
 
+//[array disStracturing ES6]
+
+// const names = ['kausar', 'jamal', 'kamal','rofiq'];
+
+// const name = names[0]
+// const name1 = names[2]
+// console.log(name);
+// console.log(name1);
+
+const [_, , , i] = ['kausar', 'jamal', 'kamal','rofiq'];
+
+console.log(_, i);
