a94fd101176b8912119e4bafb55f4803eded300e
kousra <mdkousarmia71@gmail.com>
2024-09-30 13:02:41 +0600

    js practis

---
+++ arrayForEach.js
@@ -0,0 +1,10 @@
+const array = [10, 50, 20, 40];
+
+// for(let item of array) {
+//     console.log(item);
+    
+// }
+
+array.forEach((item, index,a) => {
+    console.log('item:', item, 'index:',index,a);
+});
---
+++ optionalChanging.js
@@ -0,0 +1,20 @@
+const petOnar = {
+    contrac :{
+        phone : 1786876781,
+        email: 'mdkousarmia71@gmail.com',
+    },
+    pet:{
+        name: 'lofei',
+        info: {
+            color: 'black',
+            weagth: '2kg',
+        },
+    },
+    foote:["apple", "orange", "pepeya"]
+};
+
+
+console.log(petOnar.contrac.phone);
+console.log(petOnar.pet.info.color);
+console.log(petOnar.address?.hoesNO);
+console.log(petOnar?.foote[1]);
